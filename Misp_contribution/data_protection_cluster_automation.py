23d678257b95b66478bdb0c1c8948aa85661535c
pages497 <116439538+pages497@users.noreply.github.com>
2024-03-31 16:15:31 +0200

    Update data_protection_cluster_automation.py

--- Misp_contribution/data_protection_cluster_automation.py
+++ Misp_contribution/data_protection_cluster_automation.py
@@ -16,6 +16,39 @@ def clean_html(raw_html):
 def get_authority_name(website_url):
     if not website_url:
         return ''
+
+    # Problematic case:
+    if website_url == "https://www.argentina.gob.ar/aaip/datospersonales":
+        return 'aaip'
+    if website_url == "https://www.bahamas.gov.bs/wps/portal/public/!ut/p/b1/vZbLkqJKFEW_xQ-wyEQgYZjK-_0SgYmBIgiioKg8vr6pG3dQ9o1bNemWHBAEG9bZ-5wkIGIiJOJL8izy5F7Ul6T6vI6Z7QJIBsYUa1ikzwEFriCjrWmgk-QkiL4KJBowQFnbOraRTFoUeH3-t9uQJjZEKEc3fVXjXMCrbXO12rmRs9yxoov0mA7M0hye7IXxndFQcV7v_ISvcilLId0HbeL1d8CEg_CAosiI2zQt4vkw0NvIE6Mop_Z9W2otn2mJf2geqvwoUspry3DT8ZuU7dJOEevxXw_gfw78o4fvM5gEP2SoEnGxO390-_MH-IALiCAJEYUQ4mgaMlNE8bcVaOQPgs8u_CP4xmI0CdCXN_gBmIoE2PWgA1gTEj4RAmrrlUOjjKfRLUE3GvxhNIWchPoaGKfRM32h9-6qaZSrDggAmKOrGDdV8fxYaTneTgN3vcQ8TyXQ-R34mooHqL8NlGiLnTJCvo1pEkgWfDdw8dZIJdsCf93hy9BQzp_v4ctOYTkG0oilOJYBFMUiIigjBvGt0vFCsG2iu5y0XGuLzkbAndE4dw0wSylputrY-awdiLEgqfmjPZCpMu_7U_sUmhg7lkFVtxg2WWQlssKrDHYyJ1trgTzwOFYO_Y7P26l0Epspf9xAFiKedCdzSTzeQJM-GVcd08y-VJxPx6ZXh8Xtmrcal9CZ2sLeqxgjuLbxwLMILdz76gLUR6JRmlUrhwMnJ52lFdewxZU2r8PqzGZM1V9u-mhY5XZxXfqCeyOdGWHK9fnw_TbSyf8OGXINA7DQRKYB_UAz_fCzBV2bruB06rGs-X4E_VNlmWU0mJxspOLUAVE85W43m_0AVMh3A-G7gejNQEy9G8i8G_juKXX-_JS-fJkYDrEQkhSAiEQLRBKBvs97fvrH4QXebccjaGLArXZ1tBQdb5V7u1w_hJKWbdlKLvf1eS05RX1Pdquxbc7LR70HHVwnlxiPdeE2izw_0zal6sc5oyhJYVv91mXr0tydjnDoJYNrDrnkgtAKiOa8fmo6rQqZ-XVRyfx1RXg2-wVyrHpU/dl4/d5/L2dBISEvZ0FBIS9nQSEh":
+        return 'Data Protection Commissioner' # Search by hand
+    if website_url == "https://www.gov.bb/Departments/data-protection-commissioner":
+        return 'Data Protection Commissioner'
+    if website_url == "https://apdp.bj/":
+        return 'apdp'
+    if website_url == "https://www.privacy.bm":
+        return 'Privacy Commissioner for Bermuda (PrivCom)'
+    if website_url == "https://cpd.by":
+        return 'cpd'
+    if website_url == "https://azop.hr/":
+        return 'azop'
+    if website_url == "https://personaldata.ge/en":
+        return 'Personal Data Protection Service (PDPS)'
+    if website_url == "https://www.moc.gov.gh/agencies/data-protection-commission-dpc":
+        return 'Data Protection Commission (DPC)'
+    if website_url == "https://odpa.gg/about-us":
+        return 'odpa'
+    if website_url == "https://www.gov.il/en/departments/the_privacy_protection_authority/govil-landing-page":
+        return 'The Privacy Protection Authority'
+    if website_url == "https://jerseyoic.org/":
+        return 'jerseyoic'
+    if website_url == "https://dzlp.mk/en":
+        return 'dzlp'
+    if website_url == "https://home.inai.org.mx/":
+        return 'inai'
+    if website_url == "https://ansice.td/":
+        return 'ansice'
+        
     
     # Parse the URL and split the path to get individual segments
     parsed_url = urlparse(website_url)
